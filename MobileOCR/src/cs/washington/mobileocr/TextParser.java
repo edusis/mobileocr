3407d5c151ac781592571c236d37e9a4a3f538a1
JoshScotland <JoshScotland@edcf3bf6-0b88-11df-ad0c-0fb4090ca1bc>
2010-01-29 09:24:23 +0000

    The up and down swipes have been corrected. Now the gestures are really responsive! TextParser is a really simple parser that parses text blocks into sentences into words.

---
+++ MobileOCR/src/cs/washington/mobileocr/TextParser.java
@@ -0,0 +1,27 @@
+package cs.washington.mobileocr;
+
+/*
+ * Team Sparkplugs (Josh Scotland and Hussein Yapit)
+ * This class parses the text into an array that will be read using TTS
+ */
+
+public class TextParser {
+	
+	/* 
+	 * Given sentences, will parse them.
+	 */
+	public static String[] textParse(String passedString) {
+		String delims = "[.?!]+";
+		String[] tokens = passedString.split(delims);
+		return tokens;
+	}
+	
+	/*
+	 * Given a sentence, will parse out the words
+	 */
+	public static String[] sentenceParse(String passedString) {
+		String delims = "[ ]+";
+		String[] tokens = passedString.split(delims);
+		return tokens;
+	}
+}
