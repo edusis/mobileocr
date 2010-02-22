9d1f1df8ddf28c46b306180bbd4c2679434929f2
JoshScotland <JoshScotland@edcf3bf6-0b88-11df-ad0c-0fb4090ca1bc>
2010-02-22 08:25:20 +0000

    Worked on things. Renamed app to Mobile OCR on the phone. tts_init welcomes user to app and tells them to tap and hold for instructions.
    Tried to add a tap and launch new activity but it doesn't work because of the static reference can't be used on the intents start activity. ..... So I don't know what to do for that.
    
    Started looking at the camera and how it works. Cleaned it up a little.
    
    Made the screenreader launch by touching the MENU key since I can't get the tap to work.
    
    Double checked the screenreader's lifecycle. It doesn't need modifications because it goes through a whole lifecycle. I did add an on resume to let the user know they are in the screenreader.

--- mobileOCR/src/cs/washington/mobileocr/gestures/NavigationGestureHandler.java
+++ mobileOCR/src/cs/washington/mobileocr/gestures/NavigationGestureHandler.java
@@ -5,13 +5,24 @@
 
 package cs.washington.mobileocr.gestures;
 
-public class NavigationGestureHandler extends GestureHandler{
+import android.util.Log;
+import android.view.MotionEvent;
 
-	
+public class NavigationGestureHandler extends GestureHandler {
+
+	private static final int SWIPE_MIN_DISTANCE = 120;
+	private static final int SWIPE_MAX_OFF_PATH = 250;
+	private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+	private String TAG = this.getClass().getSimpleName();
 	
 	protected int nextState(int gestureEvent) {
-		// TODO Auto-generated method stub
 		return 0;
+	}
+	
+	public boolean onSingleTapConfirmed(MotionEvent e) {
+		Log.d(TAG,"Single Tap");
+		//MobileOCR.startScreenReaderView("Hey");
+		return false;
 	}
 
 }
