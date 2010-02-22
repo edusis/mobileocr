5f17f13730c9af393af2c59903df787c09765fec
JoshScotland <JoshScotland@edcf3bf6-0b88-11df-ad0c-0fb4090ca1bc>
2010-02-22 07:00:29 +0000

    Several small improvements. Added permissions (which must have been taken out). Removed title frame. Re added text box in screenreader for people with vision. Large text for low vision. Fixed log.e for utterance completed.

--- mobileOCR/src/cs/washington/mobileocr/main/ScreenReader.java
+++ mobileOCR/src/cs/washington/mobileocr/main/ScreenReader.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 /*
  * Josh Scotland and Hussein Yapit
@@ -23,15 +26,22 @@ public class ScreenReader extends Activity {
 		super.onCreate(savedInstanceState);
 		Log.d(TAG, "Screenreader started");
 		
+		//Remove the title frame
+		Window window = getWindow();
+		window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN |
+				WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		requestWindowFeature(Window.FEATURE_NO_TITLE); 
+		
 		setContentView(R.layout.screenreader);
-
+		TextView text = (TextView) findViewById(R.id.text);
+        text.setText(R.string.tts_passedstring);
+        
 		Bundle extras = this.getIntent().getExtras();
 		String passedString = null;
 		passedString = extras != null ? extras.getString("res"): "Look we are supposed to be winning the hearts and the minds of the natives. Isn't that the whole point of your little puppet show? You look like them and you talk like them and they will start trusting us. We built them a school, we teach them english but after that. How many years?";
-
 		TTSHandler.getInstance().ttsSetContext(this, this.getResources());
-		ScreenReaderGestureHandler gHandler = new ScreenReaderGestureHandler(passedString);
 		
+		ScreenReaderGestureHandler gHandler = new ScreenReaderGestureHandler(passedString);
 		gestureScanner = new GestureDetector(gHandler);
 	}
 
