e385e70774bad085c86e4345f61365e876694f44
yapith <yapith@edcf3bf6-0b88-11df-ad0c-0fb4090ca1bc>
2010-02-23 02:38:45 +0000

    Has network module on it, integrated with screen reader.
    start, take picture, autmatically starts screen reader that will read the captured text. pictures are taken in landscape.
    
    Note: This is a new project. The old one has weird eclipse strings.xml problem.
    
    Note: If you put your code any part of the project, like you bypass MobileOCR with startActivity(), please comment with your name above the code.
    
    Also you have been mentioning about seeing the app crashes. It would be awesome to be more specific about the it (either debug it / copy the logcat), so I can take a look.

---
+++ MobileOCR_R2/src/washington/cs/mobileocr/gestures/IGestureHandler.java
@@ -0,0 +1,8 @@
+package washington.cs.mobileocr.gestures;
+
+import android.view.MotionEvent;
+
+public interface IGestureHandler {
+	//add any function that is to be shared among gesture handler other
+	
+}
