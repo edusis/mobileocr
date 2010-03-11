5ccbe3e9b2e31d29f3e068c171cff63862cf7f7f
JoshScotland <JoshScotland@edcf3bf6-0b88-11df-ad0c-0fb4090ca1bc>
2010-03-11 02:44:35 +0000

    Added comments to three of the four packages: gestures, TTS, and server. The "main" package is going to be commented soon.
    Removed the WordSnap server list.
    
    Fixed up the log.e's that I used to debug.

--- trunk/MobileOCR_R2_Server/src/washington/cs/mobileocr/gestures/GestureHandler.java
+++ trunk/MobileOCR_R2_Server/src/washington/cs/mobileocr/gestures/GestureHandler.java
@@ -1,30 +1,28 @@
-/**
- * @author - Hussein Yapit
- * 
- */
 
 package washington.cs.mobileocr.gestures;
 
-import java.util.Timer;
+/**
+ * Copyright 2010, Josh Scotland & Hussein Yapit
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided you follow the BSD license.
+ * 
+ * 
+ * This abstract class handles all the gestures in the application.
+ */
 
-import android.os.Handler;
-import android.os.Message;
-import android.view.MotionEvent;
 import android.view.GestureDetector.SimpleOnGestureListener;
 import washington.cs.mobileocr.main.R;
 
 public abstract class GestureHandler extends SimpleOnGestureListener implements IGestureHandler{
-	
+
 	protected int mCurrentState;
-	protected Timer mStateTimer; //may be handy for input that requires timing
-	
-	public GestureHandler()
-	{
+
+	public GestureHandler() {
 		mCurrentState = R.id.state_idle;
-		mStateTimer = new Timer();
-		//ttsHandler = TTSServiceThread.getInstance().getHandler();
 	}
-	
-	//update state
+
+	// Update state
 	protected abstract int nextState(int gestureEvent);
 }
--- trunk/MobileOCR_R2_Server/src/washington/cs/mobileocr/gestures/IGestureHandler.java
+++ trunk/MobileOCR_R2_Server/src/washington/cs/mobileocr/gestures/IGestureHandler.java
@@ -1,8 +1,17 @@
+
 package washington.cs.mobileocr.gestures;
 
-import android.view.MotionEvent;
+/**
+ * Copyright 2010, Josh Scotland & Hussein Yapit
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided you follow the BSD license.
+ * 
+ * 
+ * This interface handles the common gestures in the application.
+ */
 
 public interface IGestureHandler {
-	//add any function that is to be shared among gesture handler other
-	
+	//Add any function that is to be shared among gesture handler other
 }
