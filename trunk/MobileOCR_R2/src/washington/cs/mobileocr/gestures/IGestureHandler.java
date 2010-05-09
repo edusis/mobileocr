1dc299d5c4b90d00c2301269649a3620f9028668
joshscotland <joshscotland@edcf3bf6-0b88-11df-ad0c-0fb4090ca1bc>
2010-05-09 06:12:13 +0000

    Compiles but WeOCR server connect not working so removing

--- trunk/MobileOCR_R2/src/washington/cs/mobileocr/gestures/IGestureHandler.java
+++
@@ -1,8 +0,0 @@
-package washington.cs.mobileocr.gestures;
-
-import android.view.MotionEvent;
-
-public interface IGestureHandler {
-	//add any function that is to be shared among gesture handler other
-	
-}
