dc2b2305ba97471522d37c2e7b624fa0e19cb874
JoshScotland <JoshScotland@edcf3bf6-0b88-11df-ad0c-0fb4090ca1bc>
2010-05-10 10:04:17 +0000

    Fixed the two previous issues (barcode automatically starting and making the words choppy; nothing happens after going back to the scanner). 1 issue: onResume does a TTS when switching into the screen reader.

--- trunk/BarcodeScan/src/mocr/barcode/BarcodeScan.java
+++ trunk/BarcodeScan/src/mocr/barcode/BarcodeScan.java
@@ -4,10 +4,13 @@ import washington.cs.mobileocr.tts.TTSHandler;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.SurfaceView;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.View.OnClickListener;
 
-public class BarcodeScan extends Activity {
+public class BarcodeScan extends Activity implements OnClickListener {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -19,20 +22,21 @@ public class BarcodeScan extends Activity {
 				WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 		requestWindowFeature(Window.FEATURE_NO_TITLE); 
 		setContentView(R.layout.main);
+		SurfaceView sv = (SurfaceView)findViewById(R.id.sv);
+        sv.setOnClickListener(this);
 
 		TTSHandler.getInstance().ttsSetContext(this, this.getResources());
-		Intent intent = new Intent("com.google.zxing.client.android.SCAN");
-		startActivityForResult(intent, 0);
 	}
 
+	private Boolean error = false;
 	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
 		if (requestCode == 0) {
 			if (resultCode == RESULT_OK) {
 				String contents = intent.getStringExtra("SCAN_RESULT");
-				// Handle successful scan
+				error = false;
 				startScreenReaderView(contents);
 			} else if (resultCode == RESULT_CANCELED) {
-				// Unsuccessful
+				error = true;
 			}
 		}
 	}
@@ -42,5 +46,31 @@ public class BarcodeScan extends Activity {
 		Intent i = new Intent(this, ScreenReader.class);
 		i.putExtra("resultString", result);
 		startActivity (i);
+	}
+
+	public void onClick(View arg0) {
+		Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+		startActivityForResult(intent, 0);
+	}
+	
+	protected void onResume() {
+		if (error)
+			TTSHandler.ttsQueueSRMessage("Scanning failed. Tap the screen and scan a barcode.");
+		else
+			TTSHandler.ttsQueueSRMessage("Tap the screen and scan a barcode.");
+		super.onResume();
+	}
+
+	protected void onPause() {
+		super.onPause();
+	}
+
+	public void onStop() {
+		super.onDestroy();
+	}
+
+	public void onDestroy() {
+		TTSHandler.getInstance().TTSDestroy();
+		super.onDestroy();
 	}
 }
