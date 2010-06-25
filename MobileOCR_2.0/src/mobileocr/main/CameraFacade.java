606ef05ebe3b62a048a20b29e736340c6b563719
JoshScotland <JoshScotland@edcf3bf6-0b88-11df-ad0c-0fb4090ca1bc>
2010-06-25 05:55:52 +0000

    Cleanup of code. The min sdk is now correctly 5, not 4. Added the camera shutter sound back.

--- MobileOCR_2.0/src/mobileocr/main/CameraFacade.java
+++ MobileOCR_2.0/src/mobileocr/main/CameraFacade.java
@@ -43,33 +43,11 @@ public class CameraFacade extends SurfaceView implements SurfaceHolder.Callback 
 	public static final int AUTOFOCUS_UNKNOWN = 0;
 	public static final int AUTOFOCUS_SUCCESS = 1;
 	public static final int AUTOFOCUS_FAILURE = 2;
-	private Handler mUIHandler = null;
 	*/
 
 	private SurfaceHolder mHolder;
 	private Camera mCamera;
-	
-	/*
-	private int mx;
-	private int my;
-	private boolean surfaceExists; //This is true between SurfaceCreated and SurfaceDestroyed
-	private boolean mPreviewRunning; //This next parameter is true between a call of mCamera.startPreview() and mCamera.stopPreview
-	*/
-	//private MediaPlayer mp;
-
-	/*
-	//Construct a CameraFacade
-	public CameraFacade(Context context, SurfaceHolder holder, Handler UIHandler) {    
-		mHolder = holder;
-		mHolder.addCallback(this);
-		mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-		mCamera = null;
-		mx = my = 0;
-		surfaceExists = mPreviewRunning = false;
-		mUIHandler = UIHandler;
-		mp = MediaPlayer.create(context, R.raw.camera1);
-	}
-	*/
+	private MediaPlayer mp;
 	
 	public CameraFacade(Context context) {
         super(context);
@@ -79,6 +57,9 @@ public class CameraFacade extends SurfaceView implements SurfaceHolder.Callback 
         mHolder = getHolder();
         mHolder.addCallback(this);
         mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        
+        // MediaPlayer for the camera shutter sound
+        mp = MediaPlayer.create(context, R.raw.camera1);
     }
 	
     public void surfaceCreated(SurfaceHolder holder) {
@@ -90,7 +71,6 @@ public class CameraFacade extends SurfaceView implements SurfaceHolder.Callback 
         } catch (IOException exception) {
             mCamera.release();
             mCamera = null;
-            // TODO: add more exception handling logic here
         }
     }
     
@@ -102,7 +82,6 @@ public class CameraFacade extends SurfaceView implements SurfaceHolder.Callback 
         mCamera.release();
         mCamera = null;
     }
-
 
     private Size getOptimalPreviewSize(List<Size> sizes, int w, int h) {
         final double ASPECT_TOLERANCE = 0.05;
@@ -168,7 +147,7 @@ public class CameraFacade extends SurfaceView implements SurfaceHolder.Callback 
 
     PictureCallback jpegCallback = new PictureCallback() {
     	public void onPictureTaken(byte[] _data, Camera _camera) {
-    		// TODO Do something with the image JPEG data.
+    		mp.start();
     		TTSHandler.ttsQueueSRMessage("Picture Taken");
     	}
     };
@@ -194,92 +173,8 @@ public class CameraFacade extends SurfaceView implements SurfaceHolder.Callback 
 		}
 		*/
 	}
-
-	/*
-	//The Surface has been created, acquire the camera and tell it where to draw.
-	public void surfaceCreated(SurfaceHolder holder) {
-		Log.i(CAMERA_TAG,"Surface Created");
-		if(mHolder == null)
-			mHolder = holder;
-		startCamera();
-		surfaceExists = true;
-	}
-	*/
 	
 	/*
-	//Now that the size is known, set up the camera parameters and begin the preview
-	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-		Log.i(CAMERA_TAG,"call to surfaceChanged()");
-		if(mCamera == null)
-			return;
-		mx = width;
-		my = height;
-		setCameraParameters();
-	}
-
-	public void startPreview() {
-		if(!mPreviewRunning)
-			mCamera.startPreview();
-		mPreviewRunning = true;
-	}
-
-	private void startCamera() {
-		if(mCamera == null)
-			mCamera = Camera.open();
-		try {
-			mCamera.setPreviewDisplay(mHolder); // throws IOException
-			mCamera.setErrorCallback(new ErrorCallback() {
-				public void onError(int code, Camera c) {
-					if(code == Camera.CAMERA_ERROR_SERVER_DIED)
-						Log.e(CAMERA_TAG,"The camera server died");
-					else
-						Log.e(CAMERA_TAG,"Unknown camera error");
-				}
-			});
-		}
-		catch(IOException ioe) {
-			mCamera.release();
-			mCamera = null;
-		}
-		mPreviewRunning = false;
-	}
-
-	//Sets the values for the camera's parameters
-	private void setCameraParameters() {
-		Camera.Parameters parameters = mCamera.getParameters();
-		parameters.setPictureSize(mx, my);
-		parameters.setPictureFormat(PixelFormat.JPEG);
-		mCamera.setParameters(parameters);
-		if(!mPreviewRunning)
-			mCamera.startPreview();
-		mPreviewRunning = true;
-		initCameraStateVariables();
-	}
-
-	public void surfaceDestroyed(SurfaceHolder holder) {
-		surfaceExists = false;
-		Log.i(CAMERA_TAG,"Surface destroyed! mPreviewRunning = " + mPreviewRunning);
-		if(mCamera == null) {
-			mPreviewRunning = false; //It probably should've been already anyways
-			return;
-		}
-		if(mPreviewRunning) {
-			mCamera.stopPreview();
-			mPreviewRunning = false;
-		}
-		Log.i(CAMERA_TAG,"We've called stopPreview() (perhaps), but not yet released the camera");
-		mCamera.release();
-		mCamera = null;
-		mHolder = null;
-	}
-
-	//Get a preview frame from the camera
-	public void getPreview(Camera.PreviewCallback callback) {
-		if(mCamera == null)
-			return;
-		mCamera.setPreviewCallback(callback);
-		Log.i(CAMERA_TAG ,callback==null?"Stopping previews":"Starting to request preview frames");
-	}
 
 	public void requestAutoFocus () {
 		if (mAutoFocusInProgress || mPreviewCaptureInProgress) {
@@ -306,7 +201,6 @@ public class CameraFacade extends SurfaceView implements SurfaceHolder.Callback 
 		mPreviewCaptureInProgress = true;
 		mCamera.setOneShotPreviewCallback(new Camera.PreviewCallback() {
 			public void onPreviewFrame(byte[] data, Camera camera) {
-				mp.start();
 				Message msg = mUIHandler.obtainMessage(R.id.msg_camera_preview_frame, data);
 				mUIHandler.sendMessage(msg);
 				mCamera.stopPreview();
@@ -314,30 +208,6 @@ public class CameraFacade extends SurfaceView implements SurfaceHolder.Callback 
 				mCamera = null;
 			}
 		});
-	}
-
-	public void clearPreviewFrame() {
-		mPreviewCaptureInProgress = false;
-	}
-
-	public Size getPreviewSize() {
-		if (mPreviewRunning) {
-			return mCamera.getParameters().getPreviewSize();
-		}
-		return null;
-	}
-
-	public int getWidth() {
-		return mx;
-	}
-
-	public int getHeight() {
-		return my;
-	}
-
-	private void initCameraStateVariables () {
-		mAutoFocusInProgress = false;
-		mPreviewCaptureInProgress = false;
 	}
 	*/
 
